9b6dd7649473cae7f4e12d8f34abdd815d395bcc
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 20:01:38 +0530

    Added: Store result in Dictonary Functionality.

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,11 +1,13 @@
 echo "Welcome to Arithmetic Computation"
 
+declare -A result
+
 #Take Input
 read -p "Enter the 1st Number: " a
 read -p "Enter the 2nd Number: " b
 read -p "Enter the 3rd Number: " c
 
-result1=$(( $a+$b*$c ))
-result2=$(( $a*$b+$c ))
-result3=`echo $a $b $c | awk '{print $3+($1/$2)}'`
-result4=$(( $a%$b+$c ))
+result["Result1"]=$(( $a+$b*$c ))
+result["Result2"]=$(( $a*$b+$c ))
+result["Result3"]=`echo $a $b $c | awk '{print $3+($1/$2)}'`
+result["Result4"]=$(( $a%$b+$c ))
