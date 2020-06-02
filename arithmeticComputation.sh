2f3d96ffc30964abe20c67b30f2fd706d428271e
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 20:25:29 +0530

    Added: Stored value into array from Dictionary.

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,6 +1,7 @@
 echo "Welcome to Arithmetic Computation"
 
 declare -A result
+declare -a resultArray
 
 #Take Input
 read -p "Enter the 1st Number: " a
@@ -11,3 +12,13 @@ result["Result1"]=$(( $a+$b*$c ))
 result["Result2"]=$(( $a*$b+$c ))
 result["Result3"]=`echo $a $b $c | awk '{print $3+($1/$2)}'`
 result["Result4"]=$(( $a%$b+$c ))
+
+counter=0
+
+echo "DDD"${result[@]}
+
+for i in ${!result[@]}
+do
+	resultArray[$counter]=${result[$i]}
+	(( counter++ ))
+done
